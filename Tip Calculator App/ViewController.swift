abaf5cc0cd48dd81b812784c50682fcc4cca4fa1
YukaNakamura10 <yuflower26@icloud.com>
2022-01-13 23:12:12 -0500

    Initial Commit

---
+++ Tip Calculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator App
+//
+//  Created by School on 13/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
